47f03aba84ff880e2e94063a9a85d604d202054e
Marco Zuñiga <marcohurtado358@gmail.com>
2022-07-01 12:13:48 -0500

    version 1.2

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -2,15 +2,6 @@ const movieService = require("../services/movie.service");
 
 const getAllMovies = async (req, res) => {
   try {
-    const { tittle } = req.query;
-
-    if (!tittle) {
-      res.status(400).json({
-        message: "Tittle query param is required",
-      });
-      return;
-    }
-
     const movies = await movieService.getAllMoviesByTittle(tittle);
     res.status(200).json({
       movies,
